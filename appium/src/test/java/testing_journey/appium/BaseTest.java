b7e05baf7c59b6dc10e8c438c8b0d542db755053
Muhammad-Ali-Mujtaba <alimujtaba01@gmail.com>
2023-08-23 10:34:56 +0200

    Test case for using cart and opening website in WebView to complete purchase.

--- appium/src/test/java/testing_journey/appium/BaseTest.java
+++ appium/src/test/java/testing_journey/appium/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -16,6 +17,7 @@ import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.android.options.context.SupportsChromeOptionsOption;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 
@@ -35,12 +37,15 @@ public class BaseTest {
 
 		service = new AppiumServiceBuilder().withAppiumJS(new File(
 				"C:\\\\Users\\\\Muhammad Ali\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\appium\\\\build\\\\lib\\\\main.js"))
-				.withIPAddress("127.0.0.1").usingPort(4723).build();
+				.withIPAddress("127.0.0.1").withTimeout(Duration.ofSeconds(50)).usingPort(4723).build();
 
 		service.start();
 
 		UiAutomator2Options options = new UiAutomator2Options();
 		options.setDeviceName("Pixel 2 API 28");
+		
+
+		//options.setChromedriverExecutable("D:\\Softwares\\chromedriver-win32\\chromedriver.exe");
 
 		if (appSelection == 1) {
 			options.setApp(
@@ -49,7 +54,9 @@ public class BaseTest {
 
 			options.setApp(
 					"C:\\Users\\Muhammad Ali\\git\\appium\\appium\\src\\test\\java\\resources\\General-Store.apk");
+			options.autoGrantPermissions();
 		}
+		
 
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- appium/src/test/java/testing_journey/appium/Ecommerce_Cart_Total_Validation.java
+++ appium/src/test/java/testing_journey/appium/Ecommerce_Cart_Total_Validation.java
@@ -3,6 +3,7 @@ package testing_journey.appium;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
